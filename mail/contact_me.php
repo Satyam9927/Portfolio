0fcec00f18397e430abc41b7704ae316b02fba4e
Satyam Nagpal <satyamupes2015@gmail.com>
2019-02-13 14:43:02 +0530

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -30,7 +30,7 @@ $mail->IsSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'satyamstuff2@gmail.com';                 // SMTP username
-$mail->Password = '';                           // SMTP password
+$mail->Password = '';                                    // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
